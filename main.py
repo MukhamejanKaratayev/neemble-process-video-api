f5a0c2897864c0ac0a3ef5bafc38db2d8b16f322
Mukhamejan Karatayev <42678345+MukhamejanKaratayev@users.noreply.github.com>
2023-06-14 13:52:12 +0600

    some upd june14

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from source.functions import transcribe, summarize
 from fastapi import HTTPException
 from typing import List
 from dotenv import load_dotenv
-import os
-import redis
+import requests
 
 # Input for data validation
 class VideoLink(BaseModel):
-    link: str = Field(..., example="https://www.youtube.com/watch?v=9bZkp7q19f0")
+    videoKey: str = Field(..., example="9bZkp7q19f0")
+    video: str = Field(..., example="https://www.youtube.com/watch?v=9bZkp7q19f0")
 
 # Ouput for data validation
 class VideoSegment(BaseModel):
@@ -17,69 +17,39 @@ class VideoSegment(BaseModel):
     end_time: float
     text: str
 
-# class Response(BaseModel):
-#     videoKey: str
-#     language: str
-#     transcription: str
-#     segments: List[VideoSegment]
-#     summary: str
-
 class Response(BaseModel):
-    key: str
+    videoKey: str
+    language: str
+    transcription: str
+    segments: List[VideoSegment]
+    summary: str
 
-client_redis = None
+# class Response(BaseModel):
+#     key: str
 
-@app.on_event("startup")
-async def startup_event():
-    load_dotenv()
-    global client_redis
-    client_redis = redis.Redis(
-        host= os.getenv("REDIS_HOST"),
-        port= int(os.getenv("REDIS_PORT")),
-        password= os.getenv("REDIS_PASSWORD"),
-    )
-
-# @app.on_event("shutdown")
-# async def shutdown_event():
-#     await redis.close()
-
-# @app.on_event('startup')
-# def load_model():
-#     model_size= "base"
-#     model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-
-@app.post('/process', response_model=Response)
-async def get_prediction(videoInput: VideoLink):
-    videoKey = videoInput.link.split("/")[-1].replace(".mp4", "")
+# response_model=Response
+@app.post('/process')
+def get_prediction(videoInput: VideoLink):
     # try:
-    transcription = transcribe(videoInput.link)
+    transcription = transcribe(videoInput.video)
     transcription_str = '\n'.join([segment['text'] for segment in transcription['transcription']])
     if transcription_str == "":
         raise HTTPException(status_code=400, detail="Invalid video link. Transcription failed.")
     summary = summarize(transcription_str)
     if summary == "":
         raise HTTPException(status_code=400, detail="Invalid video link. Summarization failed.")
+    
     result = {
-        "videoKey": videoKey,
+        "videoKey": videoInput.videoKey,
         "language": transcription['language'],
         "transcription": transcription_str,
         "segments": transcription['transcription'],
         "summary": summary
     }
-
-    # Create a unique key for this result
-    client_redis.json().set(videoKey, '$', result)
     
-    return {"key": videoKey}
-    # return {
-    #     "videoKey": videoKey,
-    #     "language": transcription['language'],
-    #     "transcription": transcription_str,
-    #     "segments": transcription['transcription'],
-    #     "summary": summary
-    # }
-         
+    # send the result to another API http://127.0.0.1:8000/ru/api/video/fastapi/ using POST method
+    res = requests.post('http://127.0.0.1:8000/ru/api/video/fastapi/', json=result)
+    return True
     # except:
     #     raise HTTPException(status_code=400, detail="Invalid video link. Process failed.")
 
--- source/__init__.py
+++ source/__init__.py
@@ -8,6 +8,15 @@ import os
 app = FastAPI(title="Neemble ML API", description="Transcribe and summarize video", version="1.0")
 
 # Load model
+# @app.on_event("startup")
+# async def startup_event():
+#     load_dotenv()
+#     global client_redis
+#     client_redis = redis.Redis(
+#         host= os.getenv("REDIS_HOST"),
+#         port= int(os.getenv("REDIS_PORT")),
+#         password= os.getenv("REDIS_PASSWORD"),
+#     )
 model_size = "base"
 model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
